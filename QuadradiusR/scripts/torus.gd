854b3982e965cc3ad1fb82f0eb6365fb6aa2e717
Fruktus <fruktusek@gmail.com>
2022-02-22 12:06:05 +0100

    add static torus with basic animation and hover detection

--- QuadradiusR/scripts/torus.gd
+++ QuadradiusR/scripts/torus.gd
@@ -1,19 +1,33 @@
 extends Control
 
-onready var pickup_sfx = $PickupSFX2D
-onready var putdown_sfx = $PutdownSFX2D
-onready var light_on = $LightOn
-onready var light_off = $LightOff
+onready var pickup_sfx = $SFXGroup/PickupSFX2D
+onready var putdown_sfx = $SFXGroup/PutdownSFX2D
+onready var light_on = $LightGroup/LightOn
+onready var light_off = $LightGroup/LightOff
+
+var starting_pos: Vector2
 
 
 
+func _ready():
+	set_process(false)
+
+
+func _process(delta):
+	self.rect_global_position = get_global_mouse_position() - rect_size/2
 
 
 func _on_click(viewport, event: InputEvent, shape_idx):
 	if event is InputEventMouseButton:
 		if event.get_button_index() == BUTTON_LEFT and event.is_pressed():
-			print(viewport, event, shape_idx)
 			pickup_sfx.play()
+			starting_pos = rect_position
+			self.rect_scale = Vector2(1.1, 1.1)
+			set_process(true)
+		else:
+			self.rect_scale = Vector2(1, 1)
+			set_process(false)
+			rect_position = starting_pos
 
 
 func _on_hover_start():

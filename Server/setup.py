400e8b6b2e5619fb521e393fe188a9cd41034e29
Kamil Jarosz <kjarosh256@gmail.com>
2022-04-24 01:10:29 +0200

    Server: configure setuptools-scm

--- Server/setup.py
+++ Server/setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup
 
 if __name__ == '__main__':
-    setup()
+    setup(
+        use_scm_version={
+            'root': '..',
+            'fallback_version': '0.0.0',
+        },
+        setup_requires=['setuptools_scm'],
+    )
